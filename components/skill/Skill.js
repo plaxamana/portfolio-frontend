2446c97c32acedb0388537df1b718b63cb3a5e39
Philip Laxamana <philip.laxamana@hotmail.com>
2021-01-26 20:26:26 -0500

    fixed skills container

--- components/skill/Skill.js
+++ components/skill/Skill.js
@@ -9,8 +9,8 @@ export default function Skill({ skillImg, skillName, skillDesc }) {
                     <div class="w-3 h-3 bg-green-500 rounded"></div>
                 </div>
             </div>
-            <div className="flex flex-col items-center bg-white text-center rounded-b-xl items-stretch">
-                <img src={skillImg} alt="" className="pt-8 pb-6"/>
+            <div className="flex flex-col items-center bg-white text-center rounded-b-xl">
+                <img src={skillImg} alt="" className="pt-8 pb-6 w-1/4"/>
                 <div className="p-4 pb-6">
                     <p className="font-bold mb-4">{skillName}</p>
                     <p>{skillDesc}</p>
